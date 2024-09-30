8663e2ed7aa0c804a85530f62565b93debb9a409
simone-formazione <simone.formazione@outlook.com>
2024-09-30 11:06:35 +0200

    Added dummy comment to index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.get("/", (req, res) => {
     res.send("Homepage");
 })
 
+//this is a dummy comment
+
 app.listen(port, () => {
     console.log("Server starting...");
     console.log(`Server listening on port ${port}`);
